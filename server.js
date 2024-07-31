d8681c386a404eb77d7dc412c836618a5b2e2e9e
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-07-31 21:21:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-
-let timetable = [];
-
-app.get('/api/timetable', (req, res) => {
-    res.json(timetable);
-});
-
 
 
 
