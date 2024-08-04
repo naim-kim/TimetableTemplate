35a627676f1421c6129d0987c1364098649ab1cc
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-08-04 21:44:56 +0900

    :truck: Update: server.js

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// Create an instance of an Express application
 const app = express();
-// Use body-parser middleware to parse JSON requests
 app.use(bodyParser.json());
 
 let timetable = [];
 
-// Define GET endpoint to retrieve timetable
+// GET endpoint to retrieve timetable
 app.get('/api/timetable', (req, res) => {
-    // Send timetable array as JSON response
     res.json(timetable);
 });
 
-// Define a POST endpoint to update timetable
+// POST endpoint to update timetable
 app.post('/api/timetable', (req, res) => {
-    // Update timetable
     timetable = req.body;
-    // Send a success message(JSON)
     res.json({ message: 'Updated successfully!' });
 });
 
 const port = process.env.PORT || 3000;
-// Start server and listen on specified port
 app.listen(port, () => {
-    // Log a message to indicate that server is running
     console.log(`Server running on port ${port}`);
 });
